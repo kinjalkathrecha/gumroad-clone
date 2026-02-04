d9ddca266387c1e46fb68da1f28ecf71125679ee
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-02-04 11:29:44 +0530

    access content

--- gumroad/products/views.py
+++ gumroad/products/views.py
@@ -28,9 +28,15 @@ class ProductDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        has_access = False
+        if self.request.user.is_authenticated:
+            if product in self.request.user.userlibrary.products.all():
+                has_access = True
         context.update(
             {
                 "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY,
+                "has_access": has_access,
             },
         )
         return context
